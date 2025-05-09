e71a58317577ff0c0178cc9b845718633362d6ee
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2025-05-10 01:43:33 +0600

    Update 1.Two Sum.py

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -1,6 +1,20 @@
+Brute Force:
+------------
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range (len(nums)):
             for j in range (i+1,len(nums)):
                 if(nums[i]+nums[j]==target):
                     return [i,j]
+
+
+Better:
+-------
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range (len(nums)):
+            need = target - nums[i]
+            if(need in nums and nums.index(need)!=i):
+                return[i,nums.index(need)]
+        
+
