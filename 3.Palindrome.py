9ddd7fe898dd057dd59be5ab06c9822082c9296e
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2025-05-11 14:01:09 +0600

    Noman

--- 3.Palindrome.py
+++ 3.Palindrome.py
@@ -1,3 +1,5 @@
+With String:
+------------
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         s=str(x)
@@ -6,3 +8,21 @@ class Solution:
             return True
         else:
             return False
+
+Without String:
+---------------
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        rev = 0
+        original = x
+
+        while x > 0:
+            rem = x % 10
+            rev = rev * 10 + rem
+            x = x // 10
+
+        return original == rev
+
